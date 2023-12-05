1185d091b83552a60e312ed5f767db33285b414f
Snuwbule <snuwbule@gmail.com>
2023-12-05 20:41:40 +0700

    init commit

---
+++ src/main.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
+  
+  //disalbe menu
+  mainWindow.setMenu(null);
+  
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+//macOS command + Q
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+//bot logic
